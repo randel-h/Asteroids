b2238c9a1ab0597e638b086ee68d5b1487f9203d
randel-h <randelherzberg@gmail.com>
2025-09-03 23:06:29 -0500

    Changes to main.py

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        screen.fill(000000)
+        screen.fill((0, 0, 0))
 
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
