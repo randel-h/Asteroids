9854b8cccac5f53e99265d2a261e9ba399772b81
randel-h <randelherzberg@gmail.com>
2025-09-09 23:16:17 -0500

    fixes to player and main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -12,10 +12,12 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         screen.fill((0, 0, 0))
 
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000.0
